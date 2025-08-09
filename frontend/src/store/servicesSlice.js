66e5edde2aa06a10f5ab5d88cdc65b94d79e964a
Ajay Singh Adhikari <ajaysinghadhikari697@gmail.com>
2025-08-09 22:02:02 +0530

    Update servicesSlice.js

--- frontend/src/store/servicesSlice.js
+++ frontend/src/store/servicesSlice.js
@@ -20,6 +20,18 @@ const initialState = {
       para: "Js framework ensuring applications are visually appealing,functional and easy to maintain.",
       image: "/images/react.jpg",
     },
+    {
+      id: 3,
+      name: "MERN Developer",
+      para: "A MERN developer specializes in building full-stack web applications using MongoDB, Express.js, React, and Node.js, delivering seamless front-end and back-end integration.",
+      image: "https://thfvnext.bing.com/th/id/OIP.DqIUDu2YxS5_9khTZDUAmgHaFj?w=202&h=180&c=7&r=0&o=7&cb=thfvnext&dpr=1.1&pid=1.7&rm=3",
+    },
+    {
+      id: 3,
+      name: "Backend Developer",
+      para: "A backend developer focuses on building and managing the server-side logic, databases, and APIs that power applications.",
+      image: "https://thfvnext.bing.com/th?q=Back+End+Support+Icon&w=120&h=120&c=1&rs=1&qlt=90&r=0&cb=1&dpr=1.1&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247",
+    },
   ],
 };
 
